922e7b5770eee0b29ae1b062a2282769385ffbdb
mnurcholis <chollez69@gmail.com>
2023-01-27 10:29:23 +0700

    update dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Operator;
+use App\Models\Retribusi;
 use App\Models\Settings;
+use App\Models\SiteOperator;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +15,9 @@ class HomeController extends Controller
     {
         $page_title                 = 'DashBoard';
         $setting                    = Settings::find(1);
+        $data['total_operator']     = Operator::get()->count();
+        $data['total_site']         = SiteOperator::get()->count();
+        $data['total_retribusi']    = Retribusi::get()->count();
         $data['id']                 = 1;
 
         return view('admin.dashboard', compact(['setting', 'page_title']))->with($data);
